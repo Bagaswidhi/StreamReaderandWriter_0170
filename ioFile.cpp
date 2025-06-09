631d026ffd457d64c2139701ccddeaea013e2a42
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-06-09 14:34:38 +0700

    Menulisi file dengan perulangan while dan 'q' untuk keluar dari file

--- ioFile.cpp
+++ ioFile.cpp
@@ -10,4 +10,16 @@ int main (){
     ofstream outfile;
     //Membuka file untuk ditulisi
     outfile.open("contohfile.txt");
+
+    //Unlimited loop untuk menulis ke file
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+    while(true) {
+        cout << "- ";
+        // mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter 'q'
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }   
 }

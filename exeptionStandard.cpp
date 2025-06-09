7efff36ead7ab59aefa5207a07ee98e6ba79c7c4
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-06-09 15:32:12 +0700

    Mendeklarasikan exception dengan 1 catch dimana program ini melihat elemen array yang tidak ada

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -4,4 +4,15 @@
 using namespace std;
 
 int main (){
+    cout << "Awal Program" << endl; // Penanda 1...
+    try {
+        array<int, 3> data = {10, 20, 30}; // Membuat array dengan 3 elemen
+        cout << data.at(5) << endl; // Mengakses array elemen ke 5 (indeks 5)
+    }
+    catch (exception& e){
+        //Penangkap menggunakan obyek exception
+        cout << e.what() << endl; // Menampilkan pesan kesalahan. Akan dieksekusi karena array data hanya memiliki 3 elemen (indeks 0-2)
+    }
+    cout << "Baris Program Yang Terakhir" << endl; // Penanda 2 : bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan
+    return 0;
 }

f2891abcfc922b4615f3bb03f6b3636096992517
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-06-09 14:48:47 +0700

    Merapikan program dengan format documment

--- ioFile.cpp
+++ ioFile.cpp
@@ -3,45 +3,52 @@
 #include <string>
 using namespace std;
 
-int main (){
+int main()
+{
     string baris;
 
-    //Membuat objek output file stream
+    // Membuat objek output file stream
     ofstream outfile;
-    //Membuka file untuk ditulisi
+    // Membuka file untuk ditulisi
     outfile.open("contohfile.txt");
 
-    //Unlimited loop untuk menulis ke file
+    // Unlimited loop untuk menulis ke file
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
-    while(true) {
+    while (true)
+    {
         cout << "- ";
         // mendapatkan setiap karakter dalam satu baris
         getline(cin, baris);
-        //loop akan berhenti jika anda memasukkan karakter 'q'
-        if (baris == "q") break;
-        //menulis dan memasukkan nilai dari 'baris' ke dalam file
+        // loop akan berhenti jika anda memasukkan karakter 'q'
+        if (baris == "q")
+            break;
+        // menulis dan memasukkan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
     // selesai dalam menulis sekarang tutup filenya
     outfile.close();
-    
+
     // membuat objek input file stream
     ifstream infile;
     // membuka file yang sudah ditulis
     infile.open("contohfile.txt");
 
-    cout << endl << ">= Membuka dan membaca file" << endl;
+    cout << endl
+         << ">= Membuka dan membaca file" << endl;
     // jika file ada maka
-    if (infile.is_open()){
+    if (infile.is_open())
+    {
         // melakukan perulangan setiap baris
-        while (getline(infile, baris)) {
+        while (getline(infile, baris))
+        {
             // menampilkan setiap baris
             cout << baris << '\n';
         }
         // tutup file tersebut setelah selesai
         infile.close();
     }
-    //jika tidak ditemukan file maka akan menampilkan pesan ini
-    else cout << "File tidak ditemukan!" << endl;
+    // jika tidak ditemukan file maka akan menampilkan pesan ini
+    else
+        cout << "File tidak ditemukan!" << endl;
     return 0;
 }

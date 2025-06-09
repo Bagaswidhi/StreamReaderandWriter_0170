538aa60cfa3b6a197cd90367c1179e8bfd2ca2f3
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-06-09 15:20:39 +0700

    Mendeklarasikan exception dengan 2 catch

--- manualException.cpp
+++ manualException.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main(){
-    
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 3.5; // Melemparkan sebuah integer maka
+        cout << "Pernyataan ini tidak akan diesksekusi karena exception telah dilempar." << endl;
+    }
+    catch (int a) {
+        // blok ini akan di ekseskusi jika exception yang dilempar adalah integer
+        cout << a << " : Pengecualian akan dieksekusi" << endl;
+    }
+    catch (...){
+        // Jika selain integer yang dilempar maka blok ini akan dieksekusi
+        cout << "default Pengecualian dieksekusi" << endl;
+    }
+    return 0;
 }
